3dc65a864ed20cc4eeb92fff595b9d4f3d317013
rakesh.pb <rakesh.pb@nutanix.com>
2019-08-05 18:33:55 +0530

    single AZ changes

--- main.tf
+++ main.tf
@@ -27,6 +27,9 @@ resource "google_container_node_pool" "new_container_cluster_node_pool" {
   region     = "${var.general["region"]}"
   cluster    = "${google_container_cluster.new_container_cluster.name}"
   version    = "${lookup(var.node_pool[count.index], "node_version")}"
+  node_locations = [
+    "us-west1-b",
+  ]
   node_config {
     disk_size_gb    = "${lookup(var.node_pool[count.index], "disk_size_gb", 10)}"
     disk_type       = "${lookup(var.node_pool[count.index], "disk_type", "pd-standard")}"
