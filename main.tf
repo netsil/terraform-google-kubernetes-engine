7c4571149fce4301e626fb3dcdc5595952515729
Kevin Lu <kevin.lu@nutanix.com>
2020-03-02 15:41:35 -0500

    fix

--- main.tf
+++ main.tf
@@ -134,6 +134,7 @@ resource "google_container_cluster" "new_container_cluster" {
   logging_service    = "${lookup(var.master, "logging_service", "logging.googleapis.com")}"
   
   private_cluster_config {
+    enable_private_endpoint = "${var.enable_private_endpoint}"
     enable_private_nodes   = "${var.enable_private_nodes}"
     master_ipv4_cidr_block = "${var.master_ipv4_cidr_block}"
   }
